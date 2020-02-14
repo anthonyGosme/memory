663020e010d93d000a94d63cace20ec3f791f374
AnthonyGosme <anthonygosme@gmail.com>
2020-02-14 15:48:23 +0100

    V1 memory

--- src/App.js
+++ src/App.js
@@ -1,9 +1,103 @@
 import React, { Component } from 'react'
+import shuffle from 'lodash.shuffle'
+
 import './App.css'
 
+import Card from './Card'
+import GuessCount from './GuessCount'
+import HallOfFame, { FAKE_HOF } from './HallOfFame'
+const SIDE = 6
+const SYMBOLS = '😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿'
+const VISUAL_PAUSE_MSECS = 750
+
 class App extends Component {
+  state =
+    {
+      cards: this.generateCards(),
+      currentPair: [],
+      guesses: 0,
+      matchedCardIndices: []
+    }
+
+  generateCards() {
+    const result = []
+    const size = SIDE * SIDE
+    const candidates = shuffle(SYMBOLS)
+    while (result.length < size) {
+      const card = candidates.pop()
+      result.push(card, card)
+    }
+    return shuffle(result)
+  }
+
+  getFeedbackForCard(index) {
+    const { currentPair, matchedCardIndices } = this.state
+    const indexMatched = matchedCardIndices.includes(index)
+
+    if (currentPair.length < 2) {
+      return indexMatched || index === currentPair[0] ? 'visible' : 'hidden'
+    }
+
+    if (currentPair.includes(index)) {
+      return indexMatched ? 'justMatched' : 'justMismatched'
+    }
+
+    return indexMatched ? 'visible' : 'hidden'
+  }
+
+  // @autobind not availlibe in create react app
+  // so arrow  for binding this, to get a this availlaible 
+  // https://openclassrooms.com/en/courses/4664381-realisez-une-application-web-avec-react-js/4664866-faites-reference-au-bon-this-dans-vos-fonctions
+  // Arrow fx for binding
+  handleCardClick = index => {
+    const { currentPair } = this.state
+
+    if (currentPair.length === 2) {
+      return
+    }
+    }
+
+    if (currentPair.length === 0) {
+      this.setState({ currentPair: [index] })
+      return
+    }
+
+    this.handleNewPairClosedBy(index)
+  }
+
+  handleNewPairClosedBy(index) {
+    const { cards, currentPair, guesses, matchedCardIndices } = this.state
+
+    const newPair = [currentPair[0], index]
+    const newGuesses = guesses + 1
+    const matched = cards[newPair[0]] === cards[newPair[1]]
+    this.setState({ currentPair: newPair, guesses: newGuesses })
+    if (matched) {
+      this.setState({ matchedCardIndices: [...matchedCardIndices, ...newPair] })
+    }
+    setTimeout(() => this.setState({ currentPair: [] }), VISUAL_PAUSE_MSECS)
+  }
+
   render() {
-    return <div className="memory" />
+    const { cards, guesses, matchedCardIndices } = this.state
+    const won = matchedCardIndices === SIDE * SIDE
+    return (
+      <div className="memory">
+        <GuessCount guesses={guesses} />
+
+        {cards.map((card, index) => (
+          <Card
+            card={card}
+            feedback={this.getFeedbackForCard(index)}
+            key={index}
+            index={index}
+            onClick={this.handleCardClick}
+          />
+        ))}
+
+        {won && <HallOfFame entries={FAKE_HOF} />}
+      </div>
+    )
   }
 }
 
